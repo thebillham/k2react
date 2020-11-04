4c6cb36658cf49415c303a3da0dc29fd975aa02e
Ben D <thebillham@gmail.com>
2020-11-04 15:42:55 +1300

    Migration to oAuth2.0

--- src_old/actions/footprints.js
+++
@@ -1,8 +0,0 @@
-import { MARK_FOOTPRINT } from "../constants/action-types";
-
-export const markFootprint = area => dispatch => {
-  dispatch({
-    type: MARK_FOOTPRINT,
-    payload: { [area]: true }
-  });
-};
